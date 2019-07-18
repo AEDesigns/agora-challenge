ccf80e20443b4dae4270f49a8f4831121f03e374
Andrew Elliott <mtgelliott15@gmail.com>
2019-07-17 21:22:23 -0500

    Build Out Countdown Timer

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-function isChecked(){
-    if (document.getElementsByClassName('newsletter').checked){
-        alert('Thank you for subscribing');
-    } else {
-        alert('Check the checkbox before proceeding');
-    }
+let newsCheckOne = document.getElementById('firstcheck')
+let newsCheckTwo = document.getElementById('secondcheck')
+
+
+newsCheckOne.addEventListener('change', matchCheckBox, false);
+newsCheckTwo.addEventListener('change', matchCheckBox, false);
+
+
+
+function matchCheckBox(newsCheck){
+    var isChecked = newsCheck.target.checked;
+        newsCheckTwo.checked = isChecked;
+        newsCheckOne.checked = isChecked;
 }
+
+ let futureDate = new Date();
+ futureDate.setDate(futureDate.getDate() + 5);
+ futureDate.setHours(24,0,0,0);
+  setInterval(function(){
+    var NOW = new Date();
+    var millisecTilMidnight = futureDate - NOW;
+    // divide miliseconds by 1000 to get seconds
+    var second = (millisecTilMidnight / 1000) % 60;
+    var seconds = Math.trunc(second);
+    document.getElementById('seconds').innerHTML = seconds;
+    // divide seconds by 60 to get minutes
+    var minute = (millisecTilMidnight / 60000) % 60;
+    var minutes = Math.trunc(minute)
+    document.getElementById('minutes').innerHTML = minutes;
+    // divide minutes by 60 to get hours
+    var hour = (millisecTilMidnight / 3600000) % 60;
+    var hours = Math.trunc(hour);
+    document.getElementById('hours').innerHTML = hours;
+    // divide hours by 24 to get days
+    var day = (millisecTilMidnight / 86400000) % 24;
+    var days = Math.trunc(day);
+    document.getElementById('days').innerHTML = days;
+  }, 1000)
